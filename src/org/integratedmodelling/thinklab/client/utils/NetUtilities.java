e0efd7c3412474443d9c90d9edcad119d388c48b
Ferdinando Villa <ferdinando.villa@gmail.com>
2012-01-31 08:51:02 +0100

    add net utilities class

---
+++ src/org/integratedmodelling/thinklab/client/utils/NetUtilities.java
@@ -0,0 +1,36 @@
+package org.integratedmodelling.thinklab.client.utils;
+
+import java.io.IOException;
+import java.net.DatagramSocket;
+import java.net.ServerSocket;
+
+public class NetUtilities {
+
+	public static boolean portAvailable(int port) {
+
+		ServerSocket ss = null;
+	    DatagramSocket ds = null;
+	    try {
+	        ss = new ServerSocket(port);
+	        ss.setReuseAddress(true);
+	        ds = new DatagramSocket(port);
+	        ds.setReuseAddress(true);
+	        return true;
+	    } catch (IOException e) {
+	    } finally {
+	        if (ds != null) {
+	            ds.close();
+	        }
+
+	        if (ss != null) {
+	            try {
+	                ss.close();
+	            } catch (IOException e) {
+	                /* should not be thrown */
+	            }
+	        }
+	    }
+
+	    return false;
+	}
+}
